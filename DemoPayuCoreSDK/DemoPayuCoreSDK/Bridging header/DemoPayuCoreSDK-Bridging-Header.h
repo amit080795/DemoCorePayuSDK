f5f1b71e90b26866e5ab173cfa9976d01d45260f
Amit Kumar Mishra <amit080795@gmail.com>
2021-10-25 22:23:47 +0530

    new changes added install core SDK pod

---
+++ DemoPayuCoreSDK/DemoPayuCoreSDK/Bridging header/DemoPayuCoreSDK-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  DemoPayuCoreSDK-Bridging-Header.h
+//  DemoPayuCoreSDK
+//
+//  Created by Amit Kumar Mishra1 on 22/10/21.
+//
+
+#ifndef DemoPayuCoreSDK_Bridging_Header_h
+#define DemoPayuCoreSDK_Bridging_Header_h
+
+
+
+#import <PayUBizCoreKit/PayUBizCoreKit.h>
+
+#endif /* DemoPayuCoreSDK_Bridging_Header_h */
---
+++ DemoPayuCoreSDK/DemoPayuCoreSDK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoPayuCoreSDK
+//
+//  Created by Amit Kumar Mishra1 on 21/10/21.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
